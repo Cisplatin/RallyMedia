946d21b0c88dd6e44b1e376b59d60b180a5c1746
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-05-07 11:28:35 -0400

    base of project

--- README.md
+++ README.md
@@ -1,2 +1,12 @@
-# RallyMedia
+## RallyMedia
+
 A website for news of sorts.
+
+To install locally, run the following:
+
+```
+git clone https://www.github.com/Cisplatin/RallyMedia
+cd RallyMedia
+npm install && bower install
+node ./app.js
+```
---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+const port = 80;
+app.use(express.static(__dirname + '/public'));
+app.use('/bower_components', express.static('bower_components'));
+
+app.get('/', function (req, res) {
+    res.sendfile(__dirname + '/public/index.html');
+});
+
+app.listen(port, function () {
+    console.log('RallyMedia listening on port ' + port + '.');
+});
