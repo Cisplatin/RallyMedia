341ca8d73b0aad2a1410b25a7dc4cecca646c690
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-05-08 18:13:26 -0400

    added form submission kinda

--- app.js
+++ app.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
 
-const port = 80;
+const port = 3000;
+app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
 
 app.get('/', function (req, res) {
-    res.sendfile(__dirname + '/public/index.html');
+    res.sendFile(__dirname + '/public/index.html');
+});
+
+app.get('/admin', function (req, res) {
+    res.sendFile(__dirname + '/public/admin.html');
+});
+
+app.post('/admin', function (req, res) {
+    console.log(req.body);
 });
 
 app.listen(port, function () {
