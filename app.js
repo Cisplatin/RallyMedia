48e3aeddbc91c85b1b1b00807971e809c6f51869
Mitchell Van Der Hoeff <mitchellvanderhoeff@gmail.com>
2016-05-08 20:03:06 -0400

    Templatez

--- app.js
+++ app.js
@@ -5,41 +5,34 @@ const app = express();
 const Articles = require('./articles');
 const articles = new Articles();
 const bodyParser = require('body-parser');
+const exphbs  = require('express-handlebars');
 
 const port = 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
 
+app.engine('handlebars', exphbs());
+app.set('view engine', 'handlebars');
+app.set('views', './templates');
+
 app.get('/', function (req, res) {
-    res.sendFile(__dirname + '/public/index.html');
+    let data = {
+
+    };
+
+    res.render('index', data);
 });
 
 app.get('/robots.txt', function (req, res) {
     res.sendFile(__dirname + '/public/robots.txt');
 });
 
-app.get('/admin', function (req, res) {
-    res.sendFile(__dirname + '/public/admin.html');
-});
-
-app.get('/contact', function (req, res) {
-    res.sendFile(__dirname + '/public/contact.html');
-});
-
-app.get('/about', function (req, res) {
-    res.sendFile(__dirname + '/public/about.html');
-});
-
-app.post('/admin', function (req, res) {
+app.post('/post_article', function (req, res) {
     let article = req.body.article;
     articles.saveArticle(article, (err, id) => {
         res.send(err || id);
     });
-});
-
-app.get('/staff', function(req, res) {
-    res.sendFile(__dirname + '/public/staff.html');
 });
 
 app.listen(port, function () {
