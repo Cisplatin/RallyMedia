3246717906443fd9c92a588d292e7c3e70b7620b
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-05-08 20:32:40 -0400

    dos protection

--- app.js
+++ app.js
@@ -7,11 +7,14 @@ const Articles = require('./articles');
 const articles = new Articles();
 const bodyParser = require('body-parser');
 const exphbs  = require('express-handlebars');
+const Ddos = require('ddos');
+const ddos = new Ddos({'silentStart' : true});
 
 const port = process.env.PORT || 3000;
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
+app.use(ddos.express);
 
 app.engine('handlebars', exphbs());
 app.set('view engine', 'handlebars');
