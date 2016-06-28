0ea787b920a2c25421eb739e37f46b1ffa930363
Simon Hajjar <simon.j.hajjar@gmail.com>
2016-06-28 12:53:11 -0400

    removed ddos

--- app.js
+++ app.js
@@ -7,8 +7,6 @@ const app = express();
 const bodyParser = require('body-parser');
 const exphbs  = require('express-handlebars');
 const session = require('express-session');
-const Ddos = require('ddos');
-const ddos = new Ddos({'silentStart' : true});
 
 const port = process.env.PORT || 3000;
 
@@ -17,7 +15,6 @@ app.use(session({secret: process.env.SECRET}));
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static(__dirname + '/public'));
 app.use('/bower_components', express.static('bower_components'));
-app.use(ddos.express);
 app.use(require('./lib/routes'));
 
 app.engine('handlebars', exphbs());
